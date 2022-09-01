dc3bf0da194a43c3496ad0825b264b89a2aafa56
nahuelcano <nahnic.cano.-@hotmail.com>
2022-08-31 23:51:50 -0300

    config conexion database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
-const app = require('./server');
+var mysql = require('mysql');
+
+
+var Conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'sql10516263',
+    user: 'root',
+    password: '',
+
+});
+
+Conexion.connect(function(error) {
+    if (error) {
+        throw error
+    } else {
+        console.log('CONNECTION SUCCESSFUL')
+    }
+
+});
+
+Conexion.end();
+
+
+
+/*const app = require('./server');
 const http = require('http').createServer(app);
-const {sequelize} = require('./database/models/index');
+const { sequelize } = require('./database/models/index');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,4 +35,4 @@ http.listen(PORT, () => {
     }).catch(error => {
         console.log('Se ha producido un error', error);
     })
-});
+});*/
