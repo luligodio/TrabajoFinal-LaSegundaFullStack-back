66a72bfc18bd16b64556b367d225ad5d3f0c7b8a
LuliGod <luligodio@gmail.com>
2022-09-09 18:40:06 -0300

    Update 20220909181219-user.js

--- src/database/seeders/20220909181219-user.js
+++ src/database/seeders/20220909181219-user.js
@@ -4,18 +4,21 @@ module.exports = {
     async up(queryInterface, Sequelize) {
         await Promise.all([
             queryInterface.bulkInsert('productos', [{
-                    name: 'Contenido general',
-                    precio: 1,
-                    stock: 150,
-                    createdAt: new Date(),
-                    updatedAt: new Date()
-                }
+                name: 'Contenido general',
+                precio: 1,
+                stock: 150,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
 
-            ], ),
+            ],),
         ]);
     },
 
     async down(queryInterface, Sequelize) {
-        queryInterface.bulkDelete('user', null, {}),
+        await Promise.all([
+            queryInterface.bulkDelete('user', null, {}),
+            ]);
     }
+
 };
